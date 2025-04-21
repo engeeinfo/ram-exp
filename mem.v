fbef6fbbeec1ade116f35a866e7ee27b004c5407
PRASAD KAMBALE <101986991+engeeinfo@users.noreply.github.com>
2025-04-21 13:19:45 +0530

    mem.v

--- mem.v
+++ mem.v
@@ -1,16 +1,25 @@
-module mem(data_out,data_in,addr,wr,cs);
-parameter addr_size = 10, word_size = 8, mem_size = 1024;
-input wr,cs;
-input [addr_size-1:0] addr;
-input [word_size-1:0] data_in;
-output[word_size-1:0] data_out;
-reg[word_size-1:0] mem[mem_size-1:0];
-assign data_out = mem[addr];
-always@(wr or cs)
-if(wr) mem[addr] = data_in;
+module mem(
+    input [addr_size-1:0] addr,       // Memory address input
+    input [word_size-1:0] data_in,    // Data input to write
+    input wr,                          // Write enable
+    input cs,                          // Chip select
+    output [word_size-1:0] data_out   // Data output
+);
+    // Parameter definitions
+    parameter addr_size = 10;          // Address size (10 bits)
+    parameter word_size = 8;           // Word size (8 bits)
+    parameter mem_size = 1024;         // Memory size (1024 words)
+
+    // Declare memory as an array of size 'mem_size' with each element of size 'word_size'
+    reg [word_size-1:0] mem [mem_size-1:0];
+
+    // Assign data output from memory at the given address
+    assign data_out = mem[addr];
+
+    // Always block triggered by address, write enable, or chip select
+    always @(posedge wr or posedge cs) begin
+        if (wr && cs) begin
+            mem[addr] = data_in;  // Write data to memory at the given address
+        end
+    end
 endmodule
-
-
-
-
-
